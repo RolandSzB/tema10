7739ead2dcd21d8d5ed9d5d28f9758ec84a3c4e7
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-09 15:01:04 +0200

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Square color="red" size="16" />
+    <Square color="green" size="8" />
+    <Square color="yellow" size="20" />
+  </div>
+</template>
+
+<script>
+import Square from "./components/Square.vue";
+
+export default {
+  components: {
+    Square,
+  },
+};
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ vite-project/src/components/Square.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="`w-${size} h-${size} bg-${color} rounded-lg relative`">
+    <div
+      class="w-0 h-0 border-solid border-transparent border-6 absolute border-t-0 border-l-6 bottom-0 left-0 transform rotate-180"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "blue", // default color if none provided
+    },
+    size: {
+      type: String,
+      default: "12", // default size if none provided
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Import Tailwind CSS utility classes here if not already done in your project */
+</style>
